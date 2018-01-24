a1ecfc157ab697d443076582445e6c1a7bc260cd
Akshar Takle <akshar07@users.noreply.github.com>
2018-01-23 16:46:56 -0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
     firebase:{
-      apiKey: "AIzaSyBfmxbDJKE_6XTWxR6_E-O-MRumF5kB5Bk",
+      apiKey: "",
       authDomain: "portland-e3d83.firebaseapp.com",
       databaseURL: "https://portland-e3d83.firebaseio.com",
       projectId: "portland-e3d83",
-      storageBucket: "portland-e3d83.appspot.com",
-      messagingSenderId: "962540200230"
+      storageBucket: "",
+      messagingSenderId: ""
   }
 };
